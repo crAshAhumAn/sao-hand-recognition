133da3c861bed843ed26f25dac1414943e71839e
crAshAhumAn <75253343+crAshAhumAn@users.noreply.github.com>
2026-05-15 11:48:25 +0800

    Update app.js

--- web/app.js
+++ web/app.js
@@ -165,10 +165,10 @@ function getHandednessLabel(results, handLandmarks, index) {
 }
 
 function resizeCanvasToDisplaySize() {
-  if (!video) return;
+  if (!videoElement.videoWidth || c!videoElement.videoHeight) return;
   
-  const width = video.videoWidth;
-  const height = video.videoHeight;
+  const width = videoElement.videoWidth;
+  const height = videoElement.videoHeight;
 
   if (canvasElement.width !== width || canvasElement.height !== height) {
     canvasElement.width = width;
@@ -333,10 +333,10 @@ function onResults(results) {
     return;
   }
 
-  resizeCanvasToDisplaySize(results.image);
+  resizeCanvasToDisplaySize();
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(results.image, 0, 0, results.image.videoWidth || results.image.width, results.image.videoHeight || results.image.height, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight, 0, 0, canvasElement.width, canvasElement.height);
 
   const detectedHands = results.multiHandLandmarks ?? [];
   if (detectedHands.length === 0) {
@@ -499,6 +499,6 @@ window.addEventListener("keydown", (event) => {
 });
 window.addEventListener("resize", resizeCanvasToDisplaySize);
 
-resizeCanvasToDisplaySize(results.image);
+resizeCanvasToDisplaySize();
 resetResults();
 resetSteps();
